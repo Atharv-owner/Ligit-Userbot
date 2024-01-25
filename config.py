b7a459dda5a5f80d881563416d45cc24d62b57e6
Atharv <80455758+Atharv-owner@users.noreply.github.com>
2024-01-25 06:14:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/TEAMxLIGIT/Ligit-Userbot")
+REPO_URL = getenv("REPO_URL", "https://github.com/Atharv-owner/Ligit-Userbot")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
